b76b8e92af320fc1104a8b6d85413461d1be13e3
Fabien Bouleau <fabien.bouleau@gmail.com>
2024-07-17 21:30:19 +0200

    Fixup and more test cases

---
+++ test_read.py
@@ -0,0 +1,33 @@
+################################################################################
+#                                                                               
+#  Copyright (C) 2019 Fabien Bouleau
+#
+#  This file is part of lzdb.
+#
+# lzdb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lzdb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with lzdb. If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import psycopg2 as pg
+from lzdb import *
+import pprint
+pp = pprint.PrettyPrinter()
+
+LZDB.traceon = True
+
+conn = pg.connect(database = 'fboule', host='localhost', user='fboule', password='fboule')
+dbms = LZDB(conn)
+
+items = dbms.getItems(param='2004')
+pp.pprint(items)
