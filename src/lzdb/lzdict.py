684fc3625508a53702d526cd89849c140ab173dd
Fabien Bouleau <fabien.bouleau@gmail.com>
2026-08-18 23:19:58 +0200

    Add CSV support

--- src/lzdb/lzdict.py
+++ src/lzdb/lzdict.py
@@ -14,6 +14,16 @@ class lzdict(dict):
             print("Parquet::Get %s" % filename)
             return pd.read_parquet(filepath)
 
+    class csv(object):
+        def get(self, name, folder = "data"):
+            filelist = glob.glob("%s/*%s*" % (folder, name))
+            if len(filelist) != 1:
+                return None
+            filepath = filelist[0]
+            filename = filepath.split('_')[0].split('/')[1]
+            print("CSV::Get %s" % filename)
+            return pd.read_csv(filepath)
+
     def __init__(self, loader = None):
         self.__loader = loader
         if loader is None:
